5aa8c76503565bee406ed0bbd70095fca3114d35
forsigner <forsigner@gmail.com>
2020-10-02 22:16:42 +0800

    feat: test in vue example

--- packages/vue/example/src/App.vue
+++ packages/vue/example/src/App.vue
@@ -1,19 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld styli="white s-500 bgBlack" msg="Welcome to Your Vue.js App" />
+    Te
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
 
 <style>
