3197c383b85c21f9b499443211a118ea3daf19e3
韩前进 <gotoanything@foxmail.com>
2020-10-19 10:12:21 +0800

    style: type note

--- packages/core/src/types/index.ts
+++ packages/core/src/types/index.ts
@@ -18,16 +18,21 @@ export interface PlainObject {
 
 export interface Plugin {
   /**
-   * 挂载到组件 props 的属性名
+   * 将要挂载到组件 props 的属性名
    */
   name: string
   version: string
   /**
-   * 挂载到组件上的属性值
+   * @param styliStyle - 表示根据指令转换出的样式
+   * @param props - 表示组件上的所有原始属性
+   * @returns 函数返回要挂载到组件 props 上的 prop 为 name 的值
    */
-  exec: (style: StyliStyle, props: PlainObject) => any
+  exec: (styliStyle: StyliStyle, props: PlainObject) => any
 }
 
+/**
+ * 可接受数组，已满足响应式需求
+ */
 export interface StyliStyle {
   [key: string]: number | string | number[] | string[]
 }
