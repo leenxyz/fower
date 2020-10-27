4c02cc489614d754c71ec3d260c3603fcd3f924e
韩前进 <gotoanything@foxmail.com>
2020-10-27 20:30:47 +0800

    refactor(core): createStyle

--- packages/core/src/api/createStyle.ts
+++ packages/core/src/api/createStyle.ts
@@ -1,14 +1,20 @@
 import { CSSProperties } from 'react'
+import { StyliUnit } from '../types'
 import { parseModifiers } from './parseModifiers'
 
 export function createStyle(...args: (string | CSSProperties)[]): CSSProperties {
   return args.reduce((t, c) => {
     if (typeof c === 'string') {
-      const props = c
-        .split(/[\s|\t|\n]+/)
-        .reduce((result, cur) => ({ ...result, [cur]: true }), {} as any)
+      const styli = c.split(/[\s|\t|\n]+/)
+      const props = styli.reduce((result, cur) => ({ ...result, [cur]: true }), {} as any)
       const { styliUnits } = parseModifiers(props)
-      return { ...t, ...styliUnits }
+
+      const style = styliUnits.reduce((style: any, cur: StyliUnit) => {
+        style[cur.attr] = cur.value
+        return style
+      }, {})
+
+      return { ...t, ...style }
     }
     return { ...t, ...c }
   }, {} as any)
