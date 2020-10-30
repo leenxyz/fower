6b3c3c3c7a96464f38f98df55d45dd428c1ef7b1
forsigner <forsigner@gmail.com>
2020-10-30 19:07:36 +0800

    chore: update example

--- packages/example/src/App.tsx
+++ packages/example/src/App.tsx
@@ -10,8 +10,32 @@ export const Text = styled('span')
 export const App = () => {
   return (
     <div className="box">
-      <View uppercase row flex-1 center bgYellow300 p-100 color="#00ff00.47">
+      <View
+        uppercase
+        row
+        flex-1
+        center
+        bgYellow300
+        p-100
+        m-10
+        bg="#00ff00"
+        w={[100, 200, 400, 800]}
+        css={{
+          fontSize: 30,
+        }}
+      >
         Hello world
+      </View>
+      <View
+        p-100
+        m-10
+        bg="#0000ff"
+        w={[100, 200, 400, 800, 1000]}
+        css={{
+          fontSize: 30,
+        }}
+      >
+        gogog
       </View>
     </div>
   )
--- packages/example/src/index.tsx
+++ packages/example/src/index.tsx
@@ -1,6 +1,6 @@
 import 'react-app-polyfill/ie11'
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-import { App } from './src/App'
+import { App } from './App'
 
 ReactDOM.render(<App />, document.getElementById('root'))
