93f14eb56e7c729b06703e6feb36963712aa98d3
forsigner <forsigner@gmail.com>
2020-11-04 15:50:25 +0800

    docs: fix build docs fail

--- website/src/theme/ReactLiveScope/index.js
+++ website/src/theme/ReactLiveScope/index.js
@@ -18,11 +18,26 @@ const ButtonExample = (props) => (
   />
 )
 
+const Button = (props) => (
+  <button
+    {...props}
+    style={{
+      backgroundColor: 'white',
+      border: 'solid red',
+      borderRadius: 20,
+      padding: 10,
+      cursor: 'pointer',
+      ...props.style,
+    }}
+  />
+)
+
 // Add react-live imports you need here
 const ReactLiveScope = {
   React,
   ...React,
   ButtonExample,
+  Button,
   createStyle,
   styled,
   Text,
