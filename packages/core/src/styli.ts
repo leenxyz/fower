efb17a591995c2c77e0af33c3cfb49365b9c7eb1
forsigner <forsigner@gmail.com>
2020-11-05 19:15:25 +0800

    fix: fix getColors

--- packages/core/src/styli.ts
+++ packages/core/src/styli.ts
@@ -24,7 +24,7 @@ class Styli {
   }
 
   getColors() {
-    return this.getTheme('colors')?.colors || {}
+    return this.getTheme('colors') || {}
   }
 
   getConfig<T = any>(type?: keyof Config): T {
