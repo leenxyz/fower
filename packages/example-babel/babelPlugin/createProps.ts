5d76538f1be1eb30d4e2866fdf3d6097ca49a3ff
Thomas Trainset <gotoanything@foxmail.com>
2020-12-15 11:48:15 +0800

    fix(babel): createProps

--- packages/example-babel/babelPlugin/createProps.ts
+++ packages/example-babel/babelPlugin/createProps.ts
@@ -3,20 +3,10 @@ import * as types from '@babel/types'
 export function createProps(attrs: types.JSXAttribute[]) {
   // create props
   const props = attrs.reduce((result: any, node: any) => {
-    const name = node.name?.name
-    const expression = node.value?.expression
-    let value = node.value?.value
+    const name = node?.name?.name
+    const { expression, value } = node?.value || {}
 
-    if (!name) return result
-
-    const isArrayExp = types.isArrayExpression(expression)
-    const isExp = types.isExpression(expression)
-
-    if (isExp && !isArrayExp) return result
-
-    if (isArrayExp) {
-      value = expression.elements.map((ele) => ele.value)
-    }
+    if (!name || types.isExpression(expression)) return result
 
     return { ...result, [name]: value || true }
   }, {})
