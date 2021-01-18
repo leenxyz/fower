fb7f3c41acb625bd9077caa7bab4a543d761452a
Thomas Trainset <gotoanything@foxmail.com>
2021-01-18 17:33:17 +0800

    fix(types): part type can not find

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,19 +1,18 @@
-export { CSSObject, PseudosObject } from './CSSObject'
-export { Atom } from './Atom'
-export { StyliPlugin, PluginCategory } from './StyliPlugin'
-export { SheetType } from './SheetType'
-export { Props } from './Props'
-export { Preset, Configuration } from './Configuration'
-
-export * from './Theme'
-export * from './StyliHTMLProps'
-export * from './StyliColor'
-export * from './AtomicProps'
-
+import { Property } from 'csstype'
 import { Theme } from './Theme'
+
+export { Property }
+export { Atom } from './Atom'
+export { Props } from './Props'
+export { Theme } from './Theme'
+export { SheetType } from './SheetType'
+export { StyliColor } from './StyliColor'
+export { AtomicProps } from './AtomicProps'
+export { CSSObject, PseudosObject } from './CSSObject'
+export { Preset, Configuration } from './Configuration'
+export { StyliPlugin, PluginCategory } from './StyliPlugin'
+export { As, ComponentProps, StyliHTMLProps } from './StyliHTMLProps'
+
 
 type FnValue = (theme: Theme) => boolean | number | string | (boolean | number | string)[]
 export type PropValue = FnValue | boolean | number | string | (boolean | number | string)[]
-
-import { Property } from 'csstype'
-export { Property }
