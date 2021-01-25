1a5cd2dc3af1234fa6c261a9deebfa662118010e
forsigner <forsigner@gmail.com>
2021-01-25 19:28:56 +0800

    refactor: remove Button component from @styli/react

--- packages/react/src/components.tsx
+++ packages/react/src/components.tsx
@@ -2,9 +2,3 @@ import { styled } from '@styli/styled'
 
 export const View = styled('div')
 export const Text = styled('span')
-export const Button = styled('button')
-export const Image = styled('img')
-export const Anchor = styled('a')
-export const Heading = styled('h2')
-export const Input = styled('input')
-export const Svg = styled('svg')
