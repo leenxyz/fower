456b08a0d29bcaa3744e7a73df5e141eb3a505cb
forsigner <forsigner@gmail.com>
2021-02-24 17:47:40 +0800

    feat: rm display-* atomic props

--- packages/styli-plugin-display/src/index.ts
+++ packages/styli-plugin-display/src/index.ts
@@ -2,18 +2,14 @@ import { StyliPlugin } from '@styli/types'
 import { kebab } from '@styli/utils'
 
 export function isDisplayKey(key: string) {
-  return /^(hide|inline|inline[Bb]lock|block|grid|table).*$|^display(-.+)?/.test(key)
+  return /^(hide|inline|inline[Bb]lock|block|grid|table)$|^display$/.test(key)
 }
 
 export function displayPropToStyle(prop: string, propValue: any): any {
-  if (prop == 'hide') return { display: 'none' }
+  if (prop === 'hide') return { display: 'none' }
 
   /** display */
-  if (/^display(-.+)?/.test(prop)) {
-    if (typeof propValue === 'string') return { display: propValue }
-
-    return { display: kebab(prop.replace(/^display-/, '')) }
-  }
+  if (prop === 'display') return { display: propValue }
 
   return { display: kebab(prop) }
 }
