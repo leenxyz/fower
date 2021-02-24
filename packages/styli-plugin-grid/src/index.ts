c15234de19eafc870951c2a6a6e4155f9f23ce99
forsigner <forsigner@gmail.com>
2021-02-24 17:45:38 +0800

    feat: create styli-plugin-grid

---
+++ packages/styli-plugin-grid/src/index.ts
@@ -0,0 +1,36 @@
+import { styli } from '@styli/core'
+import { StyliPlugin } from '@styli/types'
+import { downFirst } from '@styli/utils'
+
+function isMatch(key: string) {
+  return /^(row|column)?Gap(-.+)?$|^gridTemplateColumns(-.+)?$/i.test(key)
+}
+
+function flexPropToStyle(propKey: string, propValue: any): any {
+  const style: any = {}
+
+  if (propKey.startsWith('gridTemplateColumns')) {
+    const [, value] = propKey.split('-')
+    let columnsValue: number = value ? value : propValue
+    style.gridTemplateColumns = `repeat(${columnsValue}, minmax(0px, 1fr))`
+  }
+
+  if (/^(row|column)?Gap(-.+)?$/i.test(propKey)) {
+    const [key, value] = propKey.split('-')
+    const gapValue = value ? value : propValue
+    style[downFirst(key)] = styli.getValue(gapValue)
+  }
+
+  return style
+}
+
+export default (): StyliPlugin => {
+  return {
+    name: 'styli-plugin-grid',
+    isMatch,
+    onAtomStyleCreate(atom) {
+      atom.style = flexPropToStyle(atom.propKey, atom.propValue)
+      return atom
+    },
+  }
+}
---
+++ packages/styli-plugin-grid/types/index.d.ts
@@ -0,0 +1,58 @@
+import { StyliPlugin } from '@styli/types'
+import * as CSS from 'csstype'
+
+declare const _default: () => StyliPlugin
+export default _default
+
+declare module '@styli/types' {
+  export interface AtomicProps {
+    /**
+     * Set grid gap
+     *
+     * @example
+     * ```tsx
+     * <Box gap-10></Box>
+     * <Box gap-1rem></Box>
+     * <Box gap-10p></Box>
+     * <Box gap={10}></Box>
+     * ```
+     */
+    gap?: number | number[] | boolean
+
+    /**
+     * Set grid column gap
+     *
+     * @example
+     * ```tsx
+     * <Box columnGap-10></Box>
+     * <Box columnGap-1rem></Box>
+     * <Box columnGap-10p></Box>
+     * <Box columnGap={10}></Box>
+     * ```
+     */
+    columnGap?: number | number[] | boolean
+
+    /**
+     * Set grid row gap
+     *
+     * @example
+     * ```tsx
+     * <Box rowGap-10></Box>
+     * <Box rowGap-1rem></Box>
+     * <Box rowGap-10p></Box>
+     * <Box rowGap={10}></Box>
+     * ```
+     */
+    rowGap?: number | number[] | boolean
+
+    /**
+     * Set gridTemplateColumns
+     *
+     * @example
+     * ```tsx
+     * <Box gridTemplateColumns-4></Box>
+     * ```
+     */
+    gridTemplateColumns?: number | number[] | boolean
+  }
+}
