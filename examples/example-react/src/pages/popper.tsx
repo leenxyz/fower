68cd9bad992d9d539cc5fe9f294a9773e2752a8d
forsigner <forsigner@gmail.com>
2021-02-25 17:32:42 +0800

    feat: add popper example

---
+++ examples/example-react/src/pages/popper.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import { usePopper } from 'react-popper';
+import { Box } from '@styli/react';
+
+export default () => {
+  const [referenceElement, setReferenceElement] = useState(null);
+  const [popperElement, setPopperElement] = useState(null);
+  const [arrowElement, setArrowElement] = useState(null);
+  const { styles, attributes } = usePopper(referenceElement, popperElement, {
+    placement: 'right',
+    modifiers: [{ name: 'arrow', options: { element: arrowElement } }],
+  });
+
+  return (
+    <Box p-200>
+      <Box
+        s-100
+        bgBlue20
+        type="button"
+        ref={setReferenceElement}
+        style={{
+          // width: 100,
+          // height: 100,
+        }}
+      >
+        Reference element
+      </Box>
+
+      <Box
+        s-200
+        bgYellow20
+        ref={setPopperElement}
+        style={{
+          ...styles.popper,
+          width: 200,
+          height: 200,
+        }}
+        {...attributes.popper}
+      >
+        Popper element
+        <Box ref={setArrowElement} style={styles.arrow} />
+      </Box>
+    </Box>
+  );
+};
