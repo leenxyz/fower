ccf9e4f0b1d40a1099a83004645b6fe9fdae08b9
Thomas Trainset <gotoanything@foxmail.com>
2021-03-17 10:36:58 +0800

    refactor(plugin): display

--- packages/styli-plugin-position/src/index.ts
+++ packages/styli-plugin-position/src/index.ts
@@ -1,6 +1,5 @@
 import { styli } from '@styli/core'
 import { StyliPlugin } from '@styli/types'
-import { isValidPropValue } from '@styli/utils'
 
 export const positionKeys = ['static', 'fixed', 'absolute', 'relative', 'sticky']
 
@@ -24,10 +23,9 @@ export function positionPropToStyle(prop: string, propValue: any): any {
   }
 
   /** @example top-0, right-10, left--10... */
-  const [key, symbol = '', value] = prop.split(/\b-*?/)
-  const [, minus = ''] = symbol.split('')
-  const val = isValidPropValue(propValue) ? propValue : minus + value
-  return { [key]: styli.getValue(val) }
+  const [, matchKey, , value] = prop.match(/^(top|right|bottom|left)(-(-?[\d+A-Z]+))?$/i) || []
+  const key = matchKey.toLowerCase()
+  return { [key]: styli.getValue(value) }
 }
 
 export default (): StyliPlugin => {
