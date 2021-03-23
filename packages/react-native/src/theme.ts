dbd9c2250c9ed3afd414ecb2d2c6a81a8b5df528
forsigner <forsigner@gmail.com>
2021-03-23 23:44:33 +0800

    refactor: refactor theme fontSize

--- packages/react-native/src/theme.ts
+++ packages/react-native/src/theme.ts
@@ -5,7 +5,22 @@ export const theme: Partial<Theme> = {
   colors: colors,
   spacing: [0, 4, 8, 12, 16, 20, 24, 28, 32, 36],
   sizes: [0, 4, 8, 12, 16, 20, 24, 28, 32, 36],
-  fontSizes: [12, 14, 16, 20, 24, 32, 48, 64],
+
+  fontSize: {
+    xs: 12,
+    sm: 14,
+    base: 16,
+    lg: 18,
+    xl: 20,
+    '2xl': 24,
+    '3xl': 30,
+    '4xl': 36,
+    '5xl': 48,
+    '6xl': 60,
+    '7xl': 72,
+    '8xl': 96,
+    '9xl': 128,
+  },
   headings: [48, 32, 24, , 20, 16, 14] as string[] | number[],
   iconSizes: [16, 24, 32],
   lineHeight: {
--- packages/types/index.d.ts
+++ packages/types/index.d.ts
@@ -12,7 +12,7 @@ declare namespace StyliTypes {
     position = 'position',
     text = 'text',
     size = 'size',
-    ZIndex = 'ZIndex',
+    zIndex = 'zIndex',
     opacity = 'opacity',
     shadow = 'shadow',
     flex = 'flex',
@@ -203,9 +203,23 @@ declare namespace StyliTypes {
   interface Theme {
     breakpoints: string[] | number[]
     spacing: string[] | number[]
-    fontSizes: string[] | number[]
     headings: string[] | number[]
     iconSizes: string[] | number[]
+    fontSize: {
+      xs: string | number
+      sm: string | number
+      base: string | number
+      lg: string | number
+      xl: string | number
+      '2xl': string | number
+      '3xl': string | number
+      '4xl': string | number
+      '5xl': string | number
+      '6xl': string | number
+      '7xl': string | number
+      '8xl': string | number
+      '9xl': string | number
+    }
     lineHeight: {
       none: number | string
       tight: number | string
