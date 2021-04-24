038030660be028b95eb5b0f5ea8934b420de113b
forsigner <forsigner@gmail.com>
2021-04-24 15:37:13 +0800

    chore: add example-svelte-rollup

---
+++ examples/example-svelte-rollup/src/App.svelte
@@ -0,0 +1,50 @@
+<script>
+  export let name
+  import Nested from './Nested.svelte'
+</script>
+
+<Nested />
+<main>
+  <h1 bgGreen200 p6 toCenter>Hello xfooo {name}!</h1>
+  <div debug column toRight>
+    <div>AA</div>
+    <div>BshhhsjB</div>
+  </div>
+
+  <div debug row toCenter>
+    <div>AA</div>
+    <div>BB</div>
+  </div>
+
+  <div debug row toRight>
+    <div>AA</div>
+    <div>BB</div>
+  </div>
+
+  <p class="foo" p6 bgYellow300>
+    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte
+    apps.
+  </p>
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
---
+++ examples/example-svelte-rollup/src/Nested.svelte
@@ -0,0 +1,3 @@
+<div bgYellow200 p4 toCenter>
+  <span>...don't affect this element</span>
+</div>
