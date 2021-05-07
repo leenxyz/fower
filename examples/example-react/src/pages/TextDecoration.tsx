515b3b9d59bcebfdfcc4be4328e52a770dc951fb
forsigner <forsigner@gmail.com>
2021-05-07 17:24:25 +0800

    chore: add text decoration example

---
+++ examples/example-react/src/pages/TextDecoration.tsx
@@ -0,0 +1,31 @@
+import { Box } from '@fower/react';
+
+export default () => {
+  return (
+    <Box>
+      <Box text-30 underline>
+        40 Lorem ipsum dolor sit amet
+      </Box>
+      <Box text={40} lineThrough>
+        40 Lorem ipsum dolor sit amet
+      </Box>
+      <Box text={'20px'}>40 Lorem ipsum dolor sit amet</Box>
+      <Box text-20px>40 Lorem ipsum dolor sit amet</Box>
+      <Box text-17>Lorem ipsum dolor sit amet</Box>
+      <Box text={17}>Lorem ipsum dolor sit amet</Box>
+      <Box textXS>Lorem ipsum dolor sit amet</Box>
+      <Box textSM>Lorem ipsum dolor sit amet</Box>
+      <Box textBase>Lorem ipsum dolor sit amet</Box>
+      <Box textLG>Lorem ipsum dolor sit amet</Box>
+      <Box textXL>Lorem ipsum dolor sit amet</Box>
+      <Box text2XL>Lorem ipsum dolor sit amet</Box>
+      <Box text3XL>Lorem ipsum dolor sit amet</Box>
+      <Box text4XL>Lorem ipsum dolor sit amet</Box>
+      <Box text5XL>Lorem ipsum dolor sit amet</Box>
+      <Box text6XL>Lorem ipsum dolor sit amet</Box>
+      <Box text7XL>Lorem ipsum dolor sit amet</Box>
+      <Box text8XL>Lorem ipsum dolor sit amet</Box>
+      <Box text9XL>Lorem ipsum dolor sit amet</Box>
+    </Box>
+  );
+};
