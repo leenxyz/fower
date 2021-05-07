975bb2afa2abac77154d115cf20a928f6c62896c
forsigner <forsigner@gmail.com>
2021-05-08 00:12:00 +0800

    docs: add TextOverflow example

---
+++ examples/example-react/src/pages/TextOverflow.tsx
@@ -0,0 +1,17 @@
+import { Box } from '@fower/react';
+
+export default () => {
+  return (
+    <Box>
+      <Box w-200 textLG truncate>
+        Lorem ipsum dolor sit amet
+      </Box>
+      <Box w-200 textXL>
+        Lorem ipsum dolor sit amet
+      </Box>
+      <Box w-200 text2XL>
+        Lorem ipsum dolor sit amet
+      </Box>
+    </Box>
+  );
+};
