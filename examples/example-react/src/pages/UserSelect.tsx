3294e159aa85bb2e736485e4b6f7084802644dda
forsigner <forsigner@gmail.com>
2021-05-10 18:50:09 +0800

    feat: crate UserSelect example

---
+++ examples/example-react/src/pages/UserSelect.tsx
@@ -0,0 +1,17 @@
+import { Box } from '@fower/react';
+
+export default () => {
+  return (
+    <Box>
+      <Box text-30 selectNone>
+        Lorem ipsum dolor sit amet
+      </Box>
+      <Box text-30 selectAll>
+        Lorem ipsum dolor sit amet
+      </Box>
+      <Box text-30 selectText>
+        Lorem ipsum dolor sit amet
+      </Box>
+    </Box>
+  );
+};
