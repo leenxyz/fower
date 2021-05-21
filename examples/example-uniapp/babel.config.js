f15da9f577e08c893cc125c33bd765d5bcaf0022
doabit <doinsist@gmail.com>
2021-05-21 15:09:36 +0800

    fix: fix Delete `␍` eslint(prettier/prettier) error

--- examples/example-uniapp/babel.config.js
+++ examples/example-uniapp/babel.config.js
@@ -1,52 +1,48 @@
 const plugins = []
 
 if (process.env.UNI_OPT_TREESHAKINGNG) {
-  plugins.push(require('@dcloudio/vue-cli-plugin-uni-optimize/packages/babel-plugin-uni-api/index.js'))
+  plugins.push(
+    require('@dcloudio/vue-cli-plugin-uni-optimize/packages/babel-plugin-uni-api/index.js'),
+  )
 }
 
 if (
-  (
-    process.env.UNI_PLATFORM === 'app-plus' &&
-    process.env.UNI_USING_V8
-  ) ||
-  (
-    process.env.UNI_PLATFORM === 'h5' &&
-    process.env.UNI_H5_BROWSER === 'builtin'
-  )
+  (process.env.UNI_PLATFORM === 'app-plus' && process.env.UNI_USING_V8) ||
+  (process.env.UNI_PLATFORM === 'h5' && process.env.UNI_H5_BROWSER === 'builtin')
 ) {
   const path = require('path')
 
   const isWin = /^win/.test(process.platform)
 
-  const normalizePath = path => (isWin ? path.replace(/\\/g, '/') : path)
+  const normalizePath = (path) => (isWin ? path.replace(/\\/g, '/') : path)
 
   const input = normalizePath(process.env.UNI_INPUT_DIR)
   try {
     plugins.push([
       require('@dcloudio/vue-cli-plugin-hbuilderx/packages/babel-plugin-console'),
       {
-        file (file) {
+        file(file) {
           file = normalizePath(file)
           if (file.indexOf(input) === 0) {
             return path.relative(input, file)
           }
           return false
-        }
-      }
+        },
+      },
     ])
   } catch (e) {}
 }
 
 process.UNI_LIBRARIES = process.UNI_LIBRARIES || ['@dcloudio/uni-ui']
-process.UNI_LIBRARIES.forEach(libraryName => {
+process.UNI_LIBRARIES.forEach((libraryName) => {
   plugins.push([
     'import',
     {
-      'libraryName': libraryName,
-      'customName': (name) => {
+      libraryName: libraryName,
+      customName: (name) => {
         return `${libraryName}/lib/${name}/${name}`
-      }
-    }
+      },
+    },
   ])
 })
 module.exports = {
@@ -55,9 +51,9 @@ module.exports = {
       '@vue/app',
       {
         modules: 'commonjs',
-        useBuiltIns: process.env.UNI_PLATFORM === 'h5' ? 'usage' : 'entry'
-      }
-    ]
+        useBuiltIns: process.env.UNI_PLATFORM === 'h5' ? 'usage' : 'entry',
+      },
+    ],
   ],
-  plugins
+  plugins,
 }
