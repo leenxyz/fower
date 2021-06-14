66ae45350d0cc78777532d5400e70f7e7960e7ea
forsigner <forsigner@gmail.com>
2021-06-14 21:37:46 +0800

    test(fower-plugin-transition): test fower plugin transition

--- packages/fower-plugin-transition/src/index.test.ts
+++ packages/fower-plugin-transition/src/index.test.ts
@@ -6,8 +6,20 @@ const { isMatch, handleAtom } = plugin()
 const parser = new Parser({})
 
 test('isMatch', () => {
+  expect(isMatch!('transition')).toEqual(true)
+  expect(isMatch!('transitionNone')).toEqual(true)
+  expect(isMatch!('transitionAll')).toEqual(true)
+  expect(isMatch!('transitionCommon')).toEqual(true)
+  expect(isMatch!('transitionColors')).toEqual(true)
+  expect(isMatch!('transitionOpacity')).toEqual(true)
+  expect(isMatch!('transitionShadow')).toEqual(true)
+  expect(isMatch!('transitionTransform')).toEqual(true)
   expect(isMatch!('delay')).toEqual(true)
   expect(isMatch!('duration')).toEqual(true)
+  expect(isMatch!('easeLinear')).toEqual(true)
+  expect(isMatch!('easeIn')).toEqual(true)
+  expect(isMatch!('easeOut')).toEqual(true)
+  expect(isMatch!('easeInOut')).toEqual(true)
 })
 
 test('delay', () => {
@@ -30,4 +42,61 @@ test('duration', () => {
     parser,
   )
   expect(atom.style.transitionDuration).toEqual('100ms')
+})
+
+test('transition', () => {
+  const atom = handleAtom!(
+    new Atom({
+      propKey: 'transition',
+      propValue: 'color',
+    }),
+    parser,
+  )
+  expect(atom.style.transition).toEqual('color')
+})
+
+test('transitionAll', () => {
+  const atom = handleAtom!(
+    new Atom({
+      propKey: 'transitionAll',
+      propValue: true,
+    }),
+    parser,
+  )
+  expect(atom.style.transitionProperty).toEqual('all')
+})
+
+test('transitionNone', () => {
+  const atom = handleAtom!(
+    new Atom({
+      propKey: 'transitionNone',
+      propValue: true,
+    }),
+    parser,
+  )
+  expect(atom.style.transitionProperty).toEqual('none')
+})
+
+test('transitionColors', () => {
+  const atom = handleAtom!(
+    new Atom({
+      propKey: 'transitionColors',
+      propValue: true,
+    }),
+    parser,
+  )
+  expect(atom.style.transitionProperty).toEqual(
+    'background-color, border-color, color, fill, stroke',
+  )
+})
+
+test('easeLinear', () => {
+  const atom = handleAtom!(
+    new Atom({
+      propKey: 'easeLinear',
+      propValue: true,
+    }),
+    parser,
+  )
+  expect(atom.style.transitionTimingFunction).toEqual('linear')
 })
