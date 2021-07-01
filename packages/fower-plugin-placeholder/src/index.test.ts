f4ea78cb03ae04ee2ebc118f592db3b8aed4c50d
forsigner <forsigner@gmail.com>
2021-07-01 20:37:12 +0800

    test(plugin-placehoder): test pass

--- packages/fower-plugin-placeholder/src/index.test.ts
+++ packages/fower-plugin-placeholder/src/index.test.ts
@@ -2,7 +2,7 @@ import { setConfig } from '@fower/core'
 import { Parser } from '@fower/parser'
 import { presetWeb } from '@fower/preset-web'
 import { Atom } from '@fower/atom'
-import plugin from '.'
+import plugin from './index'
 
 setConfig(presetWeb)
 
@@ -10,107 +10,33 @@ const { isMatch, handleAtom } = plugin()
 const parser = new Parser({})
 
 test('isMatch', () => {
-  expect(isMatch!('border')).toEqual(true)
-  expect(isMatch!('borderT')).toEqual(true)
-  expect(isMatch!('borderTR')).toEqual(true)
-  expect(isMatch!('borderRed400')).toEqual(true)
+  expect(isMatch!('placeholder')).toEqual(false)
+  expect(isMatch!('placeholderGreen400')).toEqual(true)
+  expect(isMatch!('placeholderGreen400')).toEqual(true)
 })
 
-test('border', () => {
+test('placeholderGreen400', () => {
   const atom = handleAtom!(
     new Atom({
-      propKey: 'border',
+      propKey: 'placeholderGreen400',
       propValue: true,
     }),
     parser,
   )
-  expect(atom.style.borderWidth).toEqual(1)
+
+  expect(atom.meta.pseudoPrefix).toEqual('::')
+  expect(atom.meta.pseudo).toEqual('placeholder')
+  expect(atom.style.color).toEqual('green400')
 })
 
-test('border={2}', () => {
+test('placeholderInvalidColor', () => {
   const atom = handleAtom!(
     new Atom({
-      propKey: 'border',
-      propValue: 2,
-    }),
-    parser,
-  )
-  expect(atom.style.borderWidth).toEqual(2)
-})
-
-test('borderT={2}', () => {
-  const atom = handleAtom!(
-    new Atom({
-      propKey: 'borderT',
-      propValue: 2,
-    }),
-    parser,
-  )
-  expect(atom.style.borderTopWidth).toEqual(2)
-})
-
-test('borderDashed', () => {
-  const atom = handleAtom!(
-    new Atom({
-      propKey: 'borderDashed',
+      propKey: 'placeholderInvalidColor',
       propValue: true,
     }),
     parser,
   )
-  expect(atom.style.borderStyle).toEqual('dashed')
-})
 
-test('borderSolid', () => {
-  const atom = handleAtom!(
-    new Atom({
-      propKey: 'borderSolid',
-      propValue: true,
-    }),
-    parser,
-  )
-  expect(atom.style.borderStyle).toEqual('solid')
-})
-
-test('borderRed400', () => {
-  const atom = handleAtom!(
-    new Atom({
-      propKey: 'borderRed400',
-      propValue: true,
-    }),
-    parser,
-  )
-  expect(atom.style.borderColor).toEqual('red400')
-})
-
-test('borderColor="#000', () => {
-  const atom = handleAtom!(
-    new Atom({
-      propKey: 'borderColor',
-      propValue: '#000',
-    }),
-    parser,
-  )
-  expect(atom.style.borderColor).toEqual('#000')
-})
-
-test('borderNone', () => {
-  const atom = handleAtom!(
-    new Atom({
-      propKey: 'borderNone',
-      propValue: true,
-    }),
-    parser,
-  )
-  expect(atom.style.borderStyle).toEqual('none')
-})
-
-test('borderStyle="solid', () => {
-  const atom = handleAtom!(
-    new Atom({
-      propKey: 'borderStyle',
-      propValue: 'solid',
-    }),
-    parser,
-  )
-  expect(atom.style.borderStyle).toEqual('solid')
+  expect(atom.style).toBeUndefined()
 })
--- packages/fower-plugin-placeholder/src/index.ts
+++ packages/fower-plugin-placeholder/src/index.ts
@@ -7,21 +7,22 @@ function isMatch(key: string) {
   return /^placeholder.+/i.test(key)
 }
 
-function toStyle({ key }: Atom) {
+function toStyle({ key }: Atom): any {
   const colors: any = store.theme.colors
   const postfix = key.replace(/^placeholder/, '')
 
   const colorName = downFirst(postfix)
   if (colors[colorName]) return { color: colorName }
 
-  return {}
+  return undefined
 }
 
 export default (): FowerPlugin => {
   return {
     isMatch,
     handleAtom(atom) {
-      atom.meta.pseudo = '::placeholder'
+      atom.meta.pseudo = 'placeholder'
+      atom.meta.pseudoPrefix = '::'
       atom.style = toStyle(atom)
       return atom
     },
