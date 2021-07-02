72d1164e436279e4008c9f7f7de4637ed15a6b60
forsigner <forsigner@gmail.com>
2021-07-02 17:48:47 +0800

    test(plugin-svg): improve test

--- packages/fower-plugin-svg/src/index.test.ts
+++ packages/fower-plugin-svg/src/index.test.ts
@@ -1,18 +1,26 @@
+import { setConfig } from '@fower/core'
 import { Parser } from '@fower/parser'
+import { presetWeb } from '@fower/preset-web'
 import { Atom } from '@fower/atom'
-import plugin from '.'
+import plugin from './index'
 
 const { isMatch, handleAtom } = plugin()
-const parser = {} as Parser
+
+setConfig(presetWeb)
+const parser = new Parser({})
 
 test('isMatch', () => {
   expect(isMatch!('stroke')).toEqual(true)
   expect(isMatch!('fillCurrent')).toEqual(true)
   expect(isMatch!('strokeCurrent')).toEqual(true)
   expect(isMatch!('fillNone')).toEqual(true)
+  expect(isMatch!('fillRed200')).toEqual(true)
+  expect(isMatch!('fillFoo200')).toBeFalsy()
+  expect(isMatch!('fooooo')).toBeFalsy()
+  expect(isMatch!(undefined as any)).toBeFalsy()
 })
 
-test('stroke', () => {
+test('stroke widh number value', () => {
   const atom = handleAtom!(
     new Atom({
       propKey: 'stroke',
@@ -21,6 +29,17 @@ test('stroke', () => {
     parser,
   )
   expect(atom.style.strokeWidth).toEqual('1')
+})
+
+test('stroke width string value', () => {
+  const atom = handleAtom!(
+    new Atom({
+      propKey: 'stroke',
+      propValue: 'none',
+    }),
+    parser,
+  )
+  expect(atom.style.stroke).toEqual('none')
 })
 
 test('strokeCurrent', () => {
@@ -54,4 +73,16 @@ test('fillNone', () => {
     parser,
   )
   expect(atom.style.fill).toEqual('none')
+})
+
+test('fillRed200', () => {
+  const atom = handleAtom!(
+    new Atom({
+      propKey: 'fillRed200',
+      propValue: true,
+    }),
+    parser,
+  )
+  expect(atom.id).toEqual('fillRed200')
+  expect(atom.style.fill).toEqual('red200')
 })
