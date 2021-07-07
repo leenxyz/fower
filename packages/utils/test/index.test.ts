1480d44e26023ca8643ae47b2e02bcb9fddea522
forsigner <forsigner@gmail.com>
2021-07-07 15:29:26 +0800

    test(utils): improve utils test

--- packages/utils/test/index.test.ts
+++ packages/utils/test/index.test.ts
@@ -2,43 +2,76 @@ import {
   upFirst,
   downFirst,
   kebab,
+  camel,
   isNumber,
   isEmptyObj,
   isPercentNumber,
   jsKeyToCssKey,
+  objectToClassName,
+  argsToProps,
 } from '../src'
 
-describe('fower-utils', () => {
-  test('upFirst', () => {
-    expect(upFirst('abc')).toEqual('Abc')
+test('upFirst', () => {
+  expect(upFirst('abc')).toEqual('Abc')
+  expect(upFirst()).toEqual('')
+})
+
+test('downFirst', () => {
+  expect(downFirst('Abc')).toEqual('abc')
+  expect(downFirst()).toEqual('')
+})
+
+test('kebab', () => {
+  expect(kebab('aBc')).toEqual('a-bc')
+  expect(kebab('fooBar')).toEqual('foo-bar')
+  expect(kebab('FooBar')).toEqual('foo-bar')
+  expect(kebab('spaceX-1')).toEqual('space-x-1')
+  expect(kebab('p0')).toEqual('p0')
+})
+
+test('camel', () => {
+  expect(camel('FooBar')).toEqual('fooBar')
+  expect(camel('foo-bar')).toEqual('fooBar')
+  expect(camel('Foo Bar')).toEqual('fooBar')
+})
+
+test('isNumber', () => {
+  expect(isNumber('123')).toEqual(true)
+  expect(isNumber('12.3')).toEqual(true)
+})
+
+test('isEmptyObj', () => {
+  expect(isEmptyObj({})).toEqual(true)
+  expect(isEmptyObj(null)).toEqual(true)
+})
+
+test('isPercentNumber', () => {
+  expect(isPercentNumber('10p')).toEqual(true)
+})
+
+test('jsKeyToCssKey', () => {
+  expect(jsKeyToCssKey('WebkitLineClamp')).toEqual('-webkit-line-clamp')
+  expect(jsKeyToCssKey('lineHeight')).toEqual('line-height')
+})
+
+describe('objectToClassName', () => {
+  test('no prefix', () => {
+    expect(objectToClassName({ foo: 'bar' })).toEqual('css-2196683918')
   })
 
-  test('downFirst', () => {
-    expect(downFirst('Abc')).toEqual('abc')
+  test('with prefix', () => {
+    expect(objectToClassName({ foo: 'bar' }, 'foo-')).toEqual('foo-2196683918')
   })
+})
 
-  test('kebab', () => {
-    expect(kebab('aBc')).toEqual('a-bc')
-    expect(kebab('spaceX-1')).toEqual('space-x-1')
-    expect(kebab('p0')).toEqual('p0')
-  })
-
-  test('isNumber', () => {
-    expect(isNumber('123')).toEqual(true)
-    expect(isNumber('12.3')).toEqual(true)
-  })
-
-  test('isEmptyObj', () => {
-    expect(isEmptyObj({})).toEqual(true)
-    expect(isEmptyObj(null)).toEqual(true)
-  })
-
-  test('isPercentNumber', () => {
-    expect(isPercentNumber('10p')).toEqual(true)
-  })
-
-  test('jsKeyToCssKey', () => {
-    expect(jsKeyToCssKey('WebkitLineClamp')).toEqual('-webkit-line-clamp')
-    expect(jsKeyToCssKey('lineHeight')).toEqual('line-height')
+describe('argsToProps', () => {
+  test('no prefix', () => {
+    const args = ['gray100', 'className', { green100: true }, ['red100']]
+    const result = argsToProps(args)
+    expect(result).toMatchObject({
+      css: { green100: true },
+      gray100: true,
+      red100: true,
+    })
   })
 })
