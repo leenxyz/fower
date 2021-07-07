9aee928d3c425acd406987968f8640555b16fb73
forsigner <forsigner@gmail.com>
2021-07-07 11:42:33 +0800

    test(plugin-transform): improve test

--- packages/fower-plugin-transform/src/index.test.ts
+++ packages/fower-plugin-transform/src/index.test.ts
@@ -2,11 +2,16 @@ import { Parser } from '@fower/parser'
 import { Atom } from '@fower/atom'
 import plugin from '.'
 
-const { isMatch, handleAtom } = plugin()
-const parser = {} as Parser
+const { isMatch, handleAtom, afterAtomStyleCreate } = plugin()
+const parser = new Parser({})
 
 test('isMatch', () => {
   expect(isMatch!('scale')).toEqual(true)
+  expect(isMatch!('rotate')).toEqual(true)
+  expect(isMatch!('skew')).toEqual(true)
+  expect(isMatch!('origin')).toEqual(true)
+  expect(isMatch!('translateX')).toEqual(true)
+  expect(isMatch!('translateY')).toEqual(true)
 })
 
 test('scale', () => {
@@ -18,4 +23,34 @@ test('scale', () => {
     parser,
   )
   expect(atom.style).toBeFalsy()
+})
+
+describe('afterAtomStyleCreate', () => {
+  test('atom.length = 0', () => {
+    parser.atoms = []
+    afterAtomStyleCreate?.(parser)
+  })
+
+  test('atom.length > 0', () => {
+    parser.atoms = [
+      new Atom({
+        propKey: 'scale-120',
+        propValue: true,
+      }),
+    ]
+    afterAtomStyleCreate?.(parser)
+
+    expect(parser.atoms.length).toEqual(2)
+  })
+
+  test('atom with pseudo', () => {
+    parser.atoms = [
+      new Atom({
+        propKey: 'scale-120--hover',
+        propValue: true,
+      }),
+    ]
+    afterAtomStyleCreate?.(parser)
+    expect(parser.atoms.length).toEqual(2)
+  })
 })
