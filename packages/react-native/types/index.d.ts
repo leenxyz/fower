70c81d642d789f2d7ec75df1414ef77bd8699113
forsigner <forsigner@gmail.com>
2021-07-12 11:13:02 +0800

    fix: fix types in react-native

--- packages/react-native/types/index.d.ts
+++ packages/react-native/types/index.d.ts
@@ -1,130 +1,27 @@
 /// <reference types="@fower/preset-react-native" />
-import RN from 'react-native'
-export declare const ActivityIndicator: import('@fower/styled/dist/types').StyledComponent<
-  RN.ActivityIndicatorProps &
-    import('@fower/atomic-props').AtomicProps &
-    import('@fower/styled/dist/types').InjectedProps
->
-export declare const Button: import('@fower/styled/dist/types').StyledComponent<
-  RN.ButtonProps &
-    import('@fower/atomic-props').AtomicProps &
-    import('@fower/styled/dist/types').InjectedProps
->
-export declare const DrawerLayoutAndroid: import('@fower/styled/dist/types').StyledComponent<
-  RN.DrawerLayoutAndroidProps &
-    import('@fower/atomic-props').AtomicProps &
-    import('@fower/styled/dist/types').InjectedProps
->
-export declare const Image: import('@fower/styled/dist/types').StyledComponent<
-  RN.ImageProps &
-    import('@fower/atomic-props').AtomicProps &
-    import('@fower/styled/dist/types').InjectedProps
->
-export declare const ImageBackground: import('@fower/styled/dist/types').StyledComponent<
-  RN.ImageBackgroundProps &
-    import('@fower/atomic-props').AtomicProps &
-    import('@fower/styled/dist/types').InjectedProps
->
-export declare const KeyboardAvoidingView: import('@fower/styled/dist/types').StyledComponent<
-  RN.KeyboardAvoidingViewProps &
-    import('@fower/atomic-props').AtomicProps &
-    import('@fower/styled/dist/types').InjectedProps
->
-export declare const Modal: import('@fower/styled/dist/types').StyledComponent<
-  RN.ModalBaseProps &
-    RN.ModalPropsIOS &
-    RN.ModalPropsAndroid &
-    RN.ViewProps &
-    import('@fower/atomic-props').AtomicProps &
-    import('@fower/styled/dist/types').InjectedProps
->
-export declare const NavigatorIOS: import('@fower/styled/dist/types').StyledComponent<
-  RN.NavigatorIOSProps &
-    import('@fower/atomic-props').AtomicProps &
-    import('@fower/styled/dist/types').InjectedProps
->
-export declare const ScrollView: import('@fower/styled/dist/types').StyledComponent<
-  RN.ScrollViewProps &
-    import('@fower/atomic-props').AtomicProps &
-    import('@fower/styled/dist/types').InjectedProps
->
-export declare const SnapshotViewIOS: import('@fower/styled/dist/types').StyledComponent<
-  RN.SnapshotViewIOSProps &
-    import('@fower/atomic-props').AtomicProps &
-    import('@fower/styled/dist/types').InjectedProps
->
-export declare const Switch: import('@fower/styled/dist/types').StyledComponent<
-  RN.SwitchProps &
-    import('@fower/atomic-props').AtomicProps &
-    import('@fower/styled/dist/types').InjectedProps
->
-export declare const RecyclerViewBackedScrollView: import('@fower/styled/dist/types').StyledComponent<
-  RN.RecyclerViewBackedScrollViewProps &
-    import('@fower/atomic-props').AtomicProps &
-    import('@fower/styled/dist/types').InjectedProps
->
-export declare const RefreshControl: import('@fower/styled/dist/types').StyledComponent<
-  RN.RefreshControlProps &
-    import('@fower/atomic-props').AtomicProps &
-    import('@fower/styled/dist/types').InjectedProps
->
-export declare const SafeAreaView: import('@fower/styled/dist/types').StyledComponent<
-  RN.ViewProps &
-    import('@fower/atomic-props').AtomicProps &
-    import('@fower/styled/dist/types').InjectedProps
->
-export declare const StatusBar: import('@fower/styled/dist/types').StyledComponent<
-  RN.StatusBarProps &
-    import('@fower/atomic-props').AtomicProps &
-    import('@fower/styled/dist/types').InjectedProps
->
-export declare const Text: import('@fower/styled/dist/types').StyledComponent<
-  RN.TextProps &
-    import('@fower/atomic-props').AtomicProps &
-    import('@fower/styled/dist/types').InjectedProps
->
-export declare const TextInput: import('@fower/styled/dist/types').StyledComponent<
-  RN.TextInputProps &
-    import('@fower/atomic-props').AtomicProps &
-    import('@fower/styled/dist/types').InjectedProps
->
-export declare const TouchableHighlight: import('@fower/styled/dist/types').StyledComponent<
-  RN.TouchableHighlightProps &
-    import('@fower/atomic-props').AtomicProps &
-    import('@fower/styled/dist/types').InjectedProps
->
-export declare const TouchableNativeFeedback: import('@fower/styled/dist/types').StyledComponent<
-  RN.TouchableNativeFeedbackProps &
-    import('@fower/atomic-props').AtomicProps &
-    import('@fower/styled/dist/types').InjectedProps
->
-export declare const TouchableOpacity: import('@fower/styled/dist/types').StyledComponent<
-  RN.TouchableOpacityProps &
-    import('@fower/atomic-props').AtomicProps &
-    import('@fower/styled/dist/types').InjectedProps
->
-export declare const TouchableWithoutFeedback: import('@fower/styled/dist/types').StyledComponent<
-  RN.TouchableWithoutFeedbackProps &
-    import('@fower/atomic-props').AtomicProps &
-    import('@fower/styled/dist/types').InjectedProps
->
-export declare const View: import('@fower/styled/dist/types').StyledComponent<
-  RN.ViewProps &
-    import('@fower/atomic-props').AtomicProps &
-    import('@fower/styled/dist/types').InjectedProps
->
-export declare const FlatList: import('@fower/styled/dist/types').StyledComponent<
-  RN.FlatListProps<unknown> &
-    import('@fower/atomic-props').AtomicProps &
-    import('@fower/styled/dist/types').InjectedProps
->
-export declare const SectionList: import('@fower/styled/dist/types').StyledComponent<
-  RN.SectionListProps<unknown, unknown> &
-    import('@fower/atomic-props').AtomicProps &
-    import('@fower/styled/dist/types').InjectedProps
->
-export declare const VirtualizedList: import('@fower/styled/dist/types').StyledComponent<
-  RN.VirtualizedListProps<unknown> &
-    import('@fower/atomic-props').AtomicProps &
-    import('@fower/styled/dist/types').InjectedProps
->
+import RN from 'react-native';
+export declare const ActivityIndicator: import("@fower/styled").StyledComponent<RN.ActivityIndicatorProps & import("@fower/atomic-props").AtomicProps & import("@fower/styled").InjectedProps>;
+export declare const Button: import("@fower/styled").StyledComponent<RN.ButtonProps & import("@fower/atomic-props").AtomicProps & import("@fower/styled").InjectedProps>;
+export declare const DrawerLayoutAndroid: import("@fower/styled").StyledComponent<RN.DrawerLayoutAndroidProps & import("@fower/atomic-props").AtomicProps & import("@fower/styled").InjectedProps>;
+export declare const Image: import("@fower/styled").StyledComponent<RN.ImageProps & import("@fower/atomic-props").AtomicProps & import("@fower/styled").InjectedProps>;
+export declare const ImageBackground: import("@fower/styled").StyledComponent<RN.ImageBackgroundProps & import("@fower/atomic-props").AtomicProps & import("@fower/styled").InjectedProps>;
+export declare const KeyboardAvoidingView: import("@fower/styled").StyledComponent<RN.KeyboardAvoidingViewProps & import("@fower/atomic-props").AtomicProps & import("@fower/styled").InjectedProps>;
+export declare const Modal: import("@fower/styled").StyledComponent<RN.ModalBaseProps & RN.ModalPropsIOS & RN.ModalPropsAndroid & RN.ViewProps & import("@fower/atomic-props").AtomicProps & import("@fower/styled").InjectedProps>;
+export declare const NavigatorIOS: import("@fower/styled").StyledComponent<RN.NavigatorIOSProps & import("@fower/atomic-props").AtomicProps & import("@fower/styled").InjectedProps>;
+export declare const ScrollView: import("@fower/styled").StyledComponent<RN.ScrollViewProps & import("@fower/atomic-props").AtomicProps & import("@fower/styled").InjectedProps>;
+export declare const SnapshotViewIOS: import("@fower/styled").StyledComponent<RN.SnapshotViewIOSProps & import("@fower/atomic-props").AtomicProps & import("@fower/styled").InjectedProps>;
+export declare const Switch: import("@fower/styled").StyledComponent<RN.SwitchProps & import("@fower/atomic-props").AtomicProps & import("@fower/styled").InjectedProps>;
+export declare const RecyclerViewBackedScrollView: import("@fower/styled").StyledComponent<RN.RecyclerViewBackedScrollViewProps & import("@fower/atomic-props").AtomicProps & import("@fower/styled").InjectedProps>;
+export declare const RefreshControl: import("@fower/styled").StyledComponent<RN.RefreshControlProps & import("@fower/atomic-props").AtomicProps & import("@fower/styled").InjectedProps>;
+export declare const SafeAreaView: import("@fower/styled").StyledComponent<RN.ViewProps & import("@fower/atomic-props").AtomicProps & import("@fower/styled").InjectedProps>;
+export declare const StatusBar: import("@fower/styled").StyledComponent<RN.StatusBarProps & import("@fower/atomic-props").AtomicProps & import("@fower/styled").InjectedProps>;
+export declare const Text: import("@fower/styled").StyledComponent<RN.TextProps & import("@fower/atomic-props").AtomicProps & import("@fower/styled").InjectedProps>;
+export declare const TextInput: import("@fower/styled").StyledComponent<RN.TextInputProps & import("@fower/atomic-props").AtomicProps & import("@fower/styled").InjectedProps>;
+export declare const TouchableHighlight: import("@fower/styled").StyledComponent<RN.TouchableHighlightProps & import("@fower/atomic-props").AtomicProps & import("@fower/styled").InjectedProps>;
+export declare const TouchableNativeFeedback: import("@fower/styled").StyledComponent<RN.TouchableNativeFeedbackProps & import("@fower/atomic-props").AtomicProps & import("@fower/styled").InjectedProps>;
+export declare const TouchableOpacity: import("@fower/styled").StyledComponent<RN.TouchableOpacityProps & import("@fower/atomic-props").AtomicProps & import("@fower/styled").InjectedProps>;
+export declare const TouchableWithoutFeedback: import("@fower/styled").StyledComponent<RN.TouchableWithoutFeedbackProps & import("@fower/atomic-props").AtomicProps & import("@fower/styled").InjectedProps>;
+export declare const View: import("@fower/styled").StyledComponent<RN.ViewProps & import("@fower/atomic-props").AtomicProps & import("@fower/styled").InjectedProps>;
+export declare const FlatList: import("@fower/styled").StyledComponent<RN.FlatListProps<unknown> & import("@fower/atomic-props").AtomicProps & import("@fower/styled").InjectedProps>;
+export declare const SectionList: import("@fower/styled").StyledComponent<RN.SectionListProps<unknown, unknown> & import("@fower/atomic-props").AtomicProps & import("@fower/styled").InjectedProps>;
+export declare const VirtualizedList: import("@fower/styled").StyledComponent<RN.VirtualizedListProps<unknown> & import("@fower/atomic-props").AtomicProps & import("@fower/styled").InjectedProps>;
