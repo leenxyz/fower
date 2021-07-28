0eb83d20cd1febd0a3cff8aa40f743c82cfe135c
forsigner <forsigner@gmail.com>
2021-07-28 21:32:09 +0800

    docs: improve gradient docs

--- examples/example-react/src/pages/Gradient.tsx
+++ examples/example-react/src/pages/Gradient.tsx
@@ -2,28 +2,38 @@ import { Box } from '@fower/react';
 
 export default () => {
   return (
+    <Box square20 rounded bgGradient="linear-gradient(to right, #ccc, #888)" />
+  );
+  return (
+    <Box
+      square20
+      rounded
+      bgGradient="linear-gradient(to right, green200, pink500)"
+    />
+  );
+  return (
     <Box p10 spaceX3 toCenter>
-      <Box square20 rounded gradientX={['red400', 'yellow400']} />
+      <Box square20 rounded bgGradientX={['red400', 'yellow400']} />
 
       <Box
         square20
         rounded
-        gradientX={['green400', 'blue500']}
+        bgGradientX={['green400', 'blue500']}
         gradientX--hover={['pink500', 'yellow500']}
       />
 
-      <Box square20 rounded gradientX={['purple500', 'pink500', 'red500']} />
+      <Box square20 rounded bgGradientX={['purple500', 'pink500', 'red500']} />
 
-      <Box square20 rounded gradientY={['red400', 'yellow400']} />
+      <Box square20 rounded bgGradientY={['red400', 'yellow400']} />
 
       <Box
         square20
         rounded
-        gradientY={['green400', 'blue500']}
+        bgGradientY={['green400', 'blue500']}
         gradientY--hover={['pink500', 'yellow500']}
       />
 
-      <Box square20 rounded gradientY={['purple500', 'pink500', 'red500']} />
+      <Box square20 rounded bgGradientY={['purple500', 'pink500', 'red500']} />
     </Box>
   );
 };
