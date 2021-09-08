64d271da8db38105f64dacaa9878908907531ce1
Thomas Trainset <gotoanything@foxmail.com>
2021-09-08 11:07:49 +0800

    fix: fix react-native demo

--- examples/example-react-native-expo/App.tsx
+++ examples/example-react-native-expo/App.tsx
@@ -1,11 +1,25 @@
-import { StatusBar } from 'expo-status-bar'
 import React from 'react'
-import { Text, View } from '@fower/react-native'
+import { StatusBar } from 'expo-status-bar'
+import { View, Text, Image } from '@fower/react-native'
+import Jobs from './assets/jobs.jpg'
+
+const ProfileCard = () => (
+  <View toCenterY p-10 w-260 bgWhite rounded-10>
+    <Image circle-48 source={Jobs} />
+    <View ml-10>
+      <View textXL fontBold>
+        <Text>Steve Jobs</Text>
+      </View>
+      <Text gray800>Co-founder of Apple Inc.</Text>
+    </View>
+  </View>
+)
 
 export default function App() {
   return (
     <View mt20 mx10 p4 rounded bgGreen200>
       <Text red300>Open up App.tsx to start working on your app!</Text>
+      <ProfileCard />
       <StatusBar style="auto" />
     </View>
   )
