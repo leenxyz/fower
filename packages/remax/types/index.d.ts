323082cf54f789aa6d6b4f4858c5aa1c3bb9ea6d
leen <forsigner@gmail.com>
2022-02-25 11:23:37 +0800

    feat: improve types in remax

---
+++ packages/remax/types/index.d.ts
@@ -0,0 +1,55 @@
+/// <reference types="@fower/preset-remax" />
+/// <reference types="react" />
+export declare const Button: import("@fower/styled").StyledComponent<(import("remax/one").ButtonProps & import("@fower/atomic-props").AtomicProps & import("@fower/styled").InjectedProps) | (import("remax/one").ButtonProps & {
+    children?: import("react").ReactNode;
+} & import("@fower/atomic-props").AtomicProps & import("@fower/styled").InjectedProps)>;
+export declare const Form: import("@fower/styled").StyledComponent<(import("remax/one").FormProps & import("@fower/atomic-props").AtomicProps & import("@fower/styled").InjectedProps) | (import("remax/one").FormProps & {
+    children?: import("react").ReactNode;
+} & import("@fower/atomic-props").AtomicProps & import("@fower/styled").InjectedProps)>;
+export declare const Image: import("@fower/styled").StyledComponent<(import("remax/one").ImageProps & import("@fower/atomic-props").AtomicProps & import("@fower/styled").InjectedProps) | (import("remax/one").ImageProps & {
+    children?: import("react").ReactNode;
+} & import("@fower/atomic-props").AtomicProps & import("@fower/styled").InjectedProps)>;
+export declare const Input: import("@fower/styled").StyledComponent<Pick<import("remax/one").InputProps, "style" | "onInput" | "onConfirm" | "onFocus" | "onBlur" | "dataset" | "id" | "className" | "defaultValue" | "value" | "name" | "type" | "password" | "placeholder" | "placeholderStyle" | "disabled" | "maxLength" | "focus"> & Partial<Pick<import("remax/one").InputProps, never>> & Partial<Pick<{
+    'toutiao-selection-end': number;
+    'toutiao-selection-start': number;
+    'wechat-placeholder-class': string;
+    'wechat-cursor-spacing': number;
+    'wechat-confirm-type': string;
+    'wechat-confirm-hold': boolean;
+    'wechat-selection-end': number;
+    'wechat-selection-start': number;
+    'wechat-adjust-position': boolean;
+    'wechat-hold-keyboard': boolean;
+}, "toutiao-selection-end" | "toutiao-selection-start" | "wechat-placeholder-class" | "wechat-cursor-spacing" | "wechat-confirm-type" | "wechat-confirm-hold" | "wechat-selection-end" | "wechat-selection-start" | "wechat-adjust-position" | "wechat-hold-keyboard">> & import("@fower/atomic-props").AtomicProps & import("@fower/styled").InjectedProps>;
+export declare const Label: import("@fower/styled").StyledComponent<(import("remax/one").LabelProps & import("@fower/atomic-props").AtomicProps & import("@fower/styled").InjectedProps) | (import("remax/one").LabelProps & {
+    children?: import("react").ReactNode;
+} & import("@fower/atomic-props").AtomicProps & import("@fower/styled").InjectedProps)>;
+export declare const Navigator: import("@fower/styled").StyledComponent<(import("remax/one").NavigatorProps & import("@fower/atomic-props").AtomicProps & import("@fower/styled").InjectedProps) | (import("remax/one").NavigatorProps & {
+    children?: import("react").ReactNode;
+} & import("@fower/atomic-props").AtomicProps & import("@fower/styled").InjectedProps)>;
+export declare const Text: import("@fower/styled").StyledComponent<(import("remax/one").TextProps & import("@fower/atomic-props").AtomicProps & import("@fower/styled").InjectedProps) | (import("remax/one").TextProps & {
+    children?: import("react").ReactNode;
+} & import("@fower/atomic-props").AtomicProps & import("@fower/styled").InjectedProps)>;
+export declare const Textarea: import("@fower/styled").StyledComponent<Pick<import("remax/one").TextareaProps, "style" | "onInput" | "onConfirm" | "onFocus" | "onBlur" | "dataset" | "id" | "className" | "defaultValue" | "value" | "name" | "placeholder" | "placeholderStyle" | "disabled" | "maxLength" | "focus" | "autoHeight"> & Partial<Pick<import("remax/one").TextareaProps, never>> & Partial<Pick<{
+    'toutiao-selection-end': number;
+    'toutiao-selection-start': number;
+    'wechat-selection-end': number;
+    'wechat-selection-start': number;
+    'wechat-fixed': boolean;
+    'wechat-placeholder-class': string;
+    'wechat-cursor-spacing': number;
+    'wechat-cursor': number;
+    'wechat-show-confirm-bar': boolean;
+    'wechat-adjust-position': boolean;
+    'wechat-hold-keyboard': boolean;
+    'wechat-disable-default-padding': boolean;
+}, "toutiao-selection-end" | "toutiao-selection-start" | "wechat-placeholder-class" | "wechat-cursor-spacing" | "wechat-selection-end" | "wechat-selection-start" | "wechat-adjust-position" | "wechat-hold-keyboard" | "wechat-fixed" | "wechat-cursor" | "wechat-show-confirm-bar" | "wechat-disable-default-padding">> & import("@fower/atomic-props").AtomicProps & import("@fower/styled").InjectedProps>;
+export declare const View: import("@fower/styled").StyledComponent<(import("remax/one").ViewProps & import("@fower/atomic-props").AtomicProps & import("@fower/styled").InjectedProps) | (import("remax/one").ViewProps & {
+    children?: import("react").ReactNode;
+} & import("@fower/atomic-props").AtomicProps & import("@fower/styled").InjectedProps)>;
+export declare const WebView: import("@fower/styled").StyledComponent<(import("remax/one").WebViewProps & import("@fower/atomic-props").AtomicProps & import("@fower/styled").InjectedProps) | (import("remax/one").WebViewProps & {
+    children?: import("react").ReactNode;
+} & import("@fower/atomic-props").AtomicProps & import("@fower/styled").InjectedProps)>;
+export declare const Modal: import("@fower/styled").StyledComponent<Record<string, any> & {
+    children?: import("react").ReactNode;
+} & import("@fower/atomic-props").AtomicProps & import("@fower/styled").InjectedProps>;
